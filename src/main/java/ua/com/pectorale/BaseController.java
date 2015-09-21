7886ec5ab73713e08bd6bc3fab198279946d0a46
Vadym Shevchenko <spawnkiev@gmail.com>
2015-09-21 12:07:55 +0300

    init basic app

---
+++ src/main/java/ua/com/pectorale/BaseController.java
@@ -0,0 +1,29 @@
+package ua.com.pectorale;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by Администратор on 21.09.2015.
+ */
+@Controller
+public class BaseController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+
+}
---
+++ src/main/java/ua/com/pectorale/Greeting.java
@@ -0,0 +1,23 @@
+package ua.com.pectorale;
+
+/**
+ * Created by Администратор on 21.09.2015.
+ */
+public class Greeting {
+
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
